7482b1b41fde9aaa1e3365d649bd90b0de101099
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-07 04:45:05 +0300

    Add home to swagger redirect

---
+++ Catalog/Catalog.Api/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class HomeController : ControllerBase
+{
+    [Route("/")]
+    public IActionResult Index() => new RedirectResult("~/swagger");
+}
