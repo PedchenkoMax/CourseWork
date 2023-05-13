5c6bd10778fb615a011e823972f0b3d77c250645
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-13 05:53:28 +0300

    Add Migration Runner (WIP)

---
+++ Catalog/Catalog.Infrastructure/Database/MigrationRunner.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infrastructure.Database;
+
+// TODO: It works, but i don't like how it looks
+// like, in future i would be using logger 
+// and here it wouldn't use needed one
+// https://code-maze.com/dapper-migrations-fluentmigrator-aspnetcore/
+public class MigrationRunner
+{
+    public void RunMigrations(string connectionString, bool isMigrationUp = true)
+    {
+        var serviceProvider = CreateServices(connectionString);
+
+        using var scope = serviceProvider.CreateScope();
+
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        if (isMigrationUp)
+            runner.MigrateUp();
+        else
+            runner.MigrateDown(0);
+    }
+
+    private IServiceProvider CreateServices(string connectionString)
+    {
+        return new ServiceCollection()
+               .AddFluentMigratorCore()
+               .ConfigureRunner(runnerBuilder
+                   => runnerBuilder
+                      .AddPostgres()
+                      .WithGlobalConnectionString(connectionString)
+                      .ScanIn(typeof(MigrationRunner).Assembly).For.Migrations())
+               .AddLogging(lb => lb.AddFluentMigratorConsole())
+               .BuildServiceProvider(false);
+    }
+}
