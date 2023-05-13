3acf642af4e571c972b06d8da2a57b9f7fbf6c64
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-13 04:24:00 +0000

    Add ProductImageRepository and its abstraction

---
+++ Catalog/Catalog.Infrastructure/Database/Repositories/Abstractions/IProductImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Domain.Entities;
+
+namespace Catalog.Infrastructure.Database.Repositories.Abstractions;
+
+public interface IProductImageRepository
+{
+    Task<ProductImageEntity?> GetByProductIdAsync(Guid id);
+    Task<ProductImageEntity?> GetByIdAsync(Guid id);
+    Task<bool> UpdateAsync(ProductImageEntity productImage);
+    Task<bool> RemoveByIdAsync(Guid id);
+    Task<bool> AddAsync(ProductImageEntity productImage);
+}
---
+++ Catalog/Catalog.Infrastructure/Database/Repositories/ProductImageRepository.cs
@@ -0,0 +1,81 @@
+﻿using System.Data;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Dapper;
+
+namespace Catalog.Infrastructure.Database.Repositories;
+
+public class ProductImageRepository : IProductImageRepository
+{
+    private readonly IDbConnection connection;
+
+    public ProductImageRepository(DbContext context)
+    {
+        connection = context.Connection;
+    }
+
+    public async Task<ProductImageEntity?> GetByProductIdAsync(Guid id)
+    {
+        const string sql =
+            """
+            SELECT * FROM "ProductImages" WHERE "ProductId" = @Id
+            """;
+
+        var res = await connection.QuerySingleOrDefaultAsync<ProductImageEntity>(sql, new { Id = id });
+
+        return res;
+    }
+
+    public async Task<ProductImageEntity?> GetByIdAsync(Guid id)
+    {
+        const string sql =
+            """
+            SELECT * FROM "ProductImages" WHERE "Id" = @Id
+            """;
+
+        var res = await connection.QuerySingleOrDefaultAsync<ProductImageEntity>(sql, new { Id = id });
+
+        return res;
+    }
+
+    public async Task<bool> UpdateAsync(ProductImageEntity productImage)
+    {
+        const string sql =
+            """
+            UPDATE "ProductImages" SET
+                "ProductId" = @ProductId,
+                "ImageUrl" = @ImageUrl,
+                "DisplayOrder" = @DisplayOrder
+            WHERE "Id" = @Id
+            """;
+
+        var rowsAffected = await connection.ExecuteAsync(sql, productImage);
+
+        return rowsAffected > 0;
+    }
+
+    public async Task<bool> RemoveByIdAsync(Guid id)
+    {
+        const string sql =
+            """
+            DELETE FROM "ProductImages" WHERE "Id" = @Id
+            """;
+
+        var rowsAffected = await connection.ExecuteAsync(sql, new { Id = id });
+
+        return rowsAffected > 0;
+    }
+
+    public async Task<bool> AddAsync(ProductImageEntity productImage)
+    {
+        const string sql =
+            """
+            INSERT INTO "ProductImages" ("Id", "ProductId", "ImageUrl", "DisplayOrder")
+            VALUES (@Id, @ProductId, @ImageUrl, @DisplayOrder)
+            """;
+
+        var rowsAffected = await connection.ExecuteAsync(sql, productImage);
+
+        return rowsAffected > 0;
+    }
+}
