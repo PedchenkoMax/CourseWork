36aaddf1aa0be1c3a4487d0f7c7fd3f4f49183fb
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-17 09:56:48 +0300

    (#7) Use file-scoped namespace

--- Catalog/Catalog.Infrastructure/Database/Repositories/BrandRepository.cs
+++ Catalog/Catalog.Infrastructure/Database/Repositories/BrandRepository.cs
@@ -3,82 +3,81 @@ using Catalog.Domain.Entities;
 using Catalog.Infrastructure.Database.Repositories.Abstractions;
 using Dapper;
 
-namespace Catalog.Infrastructure.Database.Repositories
+namespace Catalog.Infrastructure.Database.Repositories;
+
+public class BrandRepository : IBrandRepository
 {
-    public class BrandRepository : IBrandRepository
+    private readonly IDbConnection connection;
+
+    public BrandRepository(DbContext context)
     {
-        private readonly IDbConnection connection;
+        connection = context.Connection;
+    }
 
-        public BrandRepository(DbContext context)
-        {
-            connection = context.Connection;
-        }
+    public async Task<List<BrandEntity>> GetAllAsync()
+    {
+        const string sql =
+            """
+            SELECT * FROM brands
+            """;
 
-        public async Task<List<BrandEntity>> GetAllAsync()
-        {
-            const string sql =
-                """
-                SELECT * FROM brands
-                """;
+        var res = await connection.QueryAsync<BrandEntity>(sql);
 
-            var res = await connection.QueryAsync<BrandEntity>(sql);
+        return res.ToList();
+    }
 
-            return res.ToList();
-        }
+    public async Task<BrandEntity?> GetByIdAsync(Guid id)
+    {
+        const string sql =
+            """
+            SELECT * FROM brands WHERE id = @Id
+            """;
 
-        public async Task<BrandEntity?> GetByIdAsync(Guid id)
-        {
-            const string sql =
-                """
-                SELECT * FROM brands WHERE id = @Id
-                """;
+        var res = await connection.QuerySingleOrDefaultAsync<BrandEntity>(sql, new { Id = id });
 
-            var res = await connection.QuerySingleOrDefaultAsync<BrandEntity>(sql, new { Id = id });
+        return res;
+    }
 
-            return res;
-        }
+    public async Task<bool> UpdateAsync(BrandEntity brand)
+    {
+        // TODO: only display order could be updatable  ??
+        const string sql =
+            """
+            UPDATE brands SET
+                name = @Name,
+                description = @Description,
+                image_url = @ImageUrl,
+                display_order = @DisplayOrder
+            WHERE id = @Id
+            """;
 
-        public async Task<bool> UpdateAsync(BrandEntity brand)
-        {
-            // TODO: only display order could be updatable  ??
-            const string sql =
-                """
-                UPDATE brands SET
-                    name = @Name,
-                    description = @Description,
-                    image_url = @ImageUrl,
-                    display_order = @DisplayOrder
-                WHERE id = @Id
-                """;
+        var rowsAffected = await connection.ExecuteAsync(sql, brand);
 
-            var rowsAffected = await connection.ExecuteAsync(sql, brand);
+        return rowsAffected > 0;
+    }
 
-            return rowsAffected > 0;
-        }
+    public async Task<bool> RemoveByIdAsync(Guid id)
+    {
+        const string sql =
+            """
+            DELETE FROM brands WHERE id = @Id
+            """;
 
-        public async Task<bool> RemoveByIdAsync(Guid id)
-        {
-            const string sql =
-                """
-                DELETE FROM brands WHERE id = @Id
-                """;
+        var rowsAffected = await connection.ExecuteAsync(sql, new { Id = id });
 
-            var rowsAffected = await connection.ExecuteAsync(sql, new { Id = id });
+        return rowsAffected > 0;
+    }
 
-            return rowsAffected > 0;
-        }
+    public async Task<bool> AddAsync(BrandEntity brand)
+    {
+        const string sql =
+            """
+            INSERT INTO brands (id, name, description, image_url, display_order)
+            VALUES (@Id, @Name, @Description, @ImageUrl, @DisplayOrder)
+            """;
 
-        public async Task<bool> AddAsync(BrandEntity brand)
-        {
-            const string sql =
-                """
-                INSERT INTO brands (id, name, description, image_url, display_order)
-                VALUES (@Id, @Name, @Description, @ImageUrl, @DisplayOrder)
-                """;
+        var rowsAffected = await connection.ExecuteAsync(sql, brand);
 
-            var rowsAffected = await connection.ExecuteAsync(sql, brand);
-
-            return rowsAffected > 0;
-        }
+        return rowsAffected > 0;
     }
 }
