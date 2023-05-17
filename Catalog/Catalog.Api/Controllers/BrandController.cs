795088d539b4f729ac906f570d2cb163c516012d
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-17 21:21:33 +0300

    Add controllers for each entity
    Note: endpoints for ProductImageEntity are in ProductController

---
+++ Catalog/Catalog.Api/Controllers/BrandController.cs
@@ -0,0 +1,99 @@
+﻿using Catalog.Api.DTO;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiController]
+[Route("api/brands")]
+public class BrandController : ControllerBase
+{
+    private readonly IBrandRepository brandRepository;
+
+    public BrandController(IBrandRepository brandRepository)
+    {
+        this.brandRepository = brandRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllBrands()
+    {
+        var brands = await brandRepository.GetAllAsync();
+
+        return Ok(brands);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetBrandById([FromRoute] Guid id)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var brand = await brandRepository.GetByIdAsync(id);
+
+        if (brand == null)
+            return NotFound();
+
+        return Ok(brand);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBrand([FromBody] Brand brandDto)
+    {
+        var validationResult = BrandEntity.TryCreate(
+            name: brandDto.Name,
+            description: brandDto.Description,
+            imageUrl: brandDto.ImageUrl,
+            displayOrder: brandDto.DisplayOrder,
+            out var brandEntity);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult);
+
+        var res = await brandRepository.AddAsync(brandEntity);
+
+        return Ok(res);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateBrand([FromRoute] Guid id, [FromBody] Brand brandDto)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var brand = await brandRepository.GetByIdAsync(id);
+
+        if (brand == null)
+            return NotFound();
+
+        var validationResult = brand.Update(
+            name: brandDto.Name,
+            description: brandDto.Description,
+            imageUrl: brandDto.ImageUrl,
+            displayOrder: brandDto.DisplayOrder);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult);
+
+        var res = await brandRepository.UpdateAsync(brand);
+
+        return Ok(res);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteBrand([FromRoute] Guid id)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var brand = await brandRepository.GetByIdAsync(id);
+
+        if (brand == null)
+            return NotFound();
+
+        var res = await brandRepository.RemoveByIdAsync(id);
+
+        return Ok(res);
+    }
+}
---
+++ Catalog/Catalog.Api/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Catalog.Api.DTO;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiController]
+[Route("api/categories")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryRepository categoryRepository;
+
+    public CategoryController(ICategoryRepository categoryRepository)
+    {
+        this.categoryRepository = categoryRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        var categories = await categoryRepository.GetAllAsync();
+
+        return Ok(categories);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetCategoryById([FromRoute] Guid id)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var category = await categoryRepository.GetByIdAsync(id);
+
+        if (category == null)
+            return NotFound();
+
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCategory([FromBody] Category categoryDto)
+    {
+        var validationResult = CategoryEntity.TryCreate(
+            parentCategoryId: categoryDto.ParentCategoryId,
+            name: categoryDto.Name,
+            description: categoryDto.Description,
+            imageUrl: categoryDto.ImageUrl,
+            displayOrder: categoryDto.DisplayOrder,
+            out var categoryEntity);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult);
+
+        var res = await categoryRepository.AddAsync(categoryEntity);
+
+        return Ok(res);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateCategory([FromRoute] Guid id, [FromBody] Category categoryDto)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var category = await categoryRepository.GetByIdAsync(id);
+
+        if (category == null)
+            return NotFound();
+
+        var validationResult = category.Update(
+            name: categoryDto.Name,
+            description: categoryDto.Description,
+            imageUrl: categoryDto.ImageUrl,
+            displayOrder: categoryDto.DisplayOrder);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult);
+
+        var res = await categoryRepository.UpdateAsync(category);
+
+        return Ok(res);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteCategory([FromRoute] Guid id)
+    {
+        if (id == Guid.Empty)
+            return BadRequest();
+
+        var category = await categoryRepository.GetByIdAsync(id);
+
+        if (category == null)
+            return NotFound();
+
+        var res = await categoryRepository.RemoveByIdAsync(id);
+
+        return Ok(res);
+    }
+}
