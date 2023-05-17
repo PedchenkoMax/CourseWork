99fbdf8109a6ad962c5f9274e285d96b02b3653c
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-17 21:20:40 +0300

    Add DTO for each entity

---
+++ Catalog/Catalog.Api/DTO/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Api.DTO;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public Guid ParentCategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public int DisplayOrder { get; set; }
+    public Category? ParentCategory { get; set; }
+    public List<Product>? Products { get; set; }
+}
---
+++ Catalog/Catalog.Api/DTO/ProductImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Api.DTO;
+
+public class ProductImage
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public string ImageUrl { get; set; }
+    public int DisplayOrder { get; set; }
+    public Product? Product { get; set; }
+}
