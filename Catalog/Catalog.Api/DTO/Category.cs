81067faa8508073a57f6b6b6383251ddd3df5b9b
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-18 22:34:25 +0300

    Make DTO's guids (FK) nullable to match entities

--- Catalog/Catalog.Api/DTO/Category.cs
+++ Catalog/Catalog.Api/DTO/Category.cs
@@ -3,7 +3,7 @@
 public class Category
 {
     public Guid Id { get; set; }
-    public Guid ParentCategoryId { get; set; }
+    public Guid? ParentCategoryId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string ImageUrl { get; set; }
--- Catalog/Catalog.Api/DTO/Product.cs
+++ Catalog/Catalog.Api/DTO/Product.cs
@@ -3,8 +3,8 @@
 public class Product
 {
     public Guid Id { get; set; }
-    public Guid BrandId { get; set; }
-    public Guid CategoryId { get; set; }
+    public Guid? BrandId { get; set; }
+    public Guid? CategoryId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
