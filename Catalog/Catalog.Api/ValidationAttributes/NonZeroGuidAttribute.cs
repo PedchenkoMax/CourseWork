08cd149467f78cb1dfe9913775e8fee045a30de6
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-19 19:34:31 +0300

    (#41) Add NonZeroGuidAttribute and NonZeroNullableGuidAttribute validation attributes

---
+++ Catalog/Catalog.Api/ValidationAttributes/NonZeroGuidAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Api.ValidationAttributes;
+
+public class NonZeroGuidAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not Guid guidValue || guidValue == Guid.Empty)
+        {
+            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+        }
+
+        return ValidationResult.Success!;
+    }
+}
---
+++ Catalog/Catalog.Api/ValidationAttributes/NonZeroNullableGuidAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Api.ValidationAttributes;
+
+public class NonZeroNullableGuidAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is Guid guidValue && guidValue == Guid.Empty)
+        {
+            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+        }
+
+        return ValidationResult.Success!;
+    }
+}
