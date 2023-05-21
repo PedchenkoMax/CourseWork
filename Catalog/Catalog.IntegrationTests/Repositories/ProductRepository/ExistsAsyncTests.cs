36c9c4c239e5dda767887c2b70a22d8bde139023
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-21 19:50:20 +0300

    Add integration tests for ProductRepository

---
+++ Catalog/Catalog.IntegrationTests/Repositories/ProductRepository/ExistsAsyncTests.cs
@@ -0,0 +1,45 @@
+﻿using Catalog.Infrastructure.Database;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Catalog.IntegrationTests.SeedData;
+using FluentAssertions;
+using Xunit;
+
+namespace Catalog.IntegrationTests.Repositories.ProductRepository;
+
+public class ExistsAsyncTests : IClassFixture<DatabaseFixture>
+{
+    private readonly IProductRepository productRepository;
+
+    public ExistsAsyncTests(DatabaseFixture fixture)
+    {
+        var context = new DbContext(fixture.ConnectionString);
+        productRepository = new Infrastructure.Database.Repositories.ProductRepository(context);
+    }
+
+    [Fact]
+    public async Task ExistsAsync_ShouldReturnTrue_WhenProductExists()
+    {
+        // Arrange
+        var productToCheck = ProductTestHelper.Default();
+        await productRepository.AddAsync(productToCheck);
+
+        // Act
+        var isExist = await productRepository.ExistsAsync(productToCheck.Id);
+
+        // Assert
+        isExist.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ExistsAsync_ShouldReturnFalse_WhenProductDoesNotExist()
+    {
+        // Arrange
+        var nonExistentProductId = Guid.Empty;
+
+        // Act
+        var isExist = await productRepository.ExistsAsync(nonExistentProductId);
+
+        // Assert
+        isExist.Should().BeFalse();
+    }
+}
---
+++ Catalog/Catalog.IntegrationTests/Repositories/ProductRepository/RemoveByIdAsyncTests.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.Infrastructure.Database;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Catalog.IntegrationTests.SeedData;
+using FluentAssertions;
+using Xunit;
+
+namespace Catalog.IntegrationTests.Repositories.ProductRepository;
+
+public class RemoveByIdAsyncTests : IClassFixture<DatabaseFixture>
+{
+    private readonly IProductRepository productRepository;
+
+    public RemoveByIdAsyncTests(DatabaseFixture fixture)
+    {
+        var context = new DbContext(fixture.ConnectionString);
+        productRepository = new Infrastructure.Database.Repositories.ProductRepository(context);
+    }
+
+    [Fact]
+    public async Task RemoveByIdAsync_ShouldReturnTrue_WhenProductExistsAndIsRemoved()
+    {
+        // Arrange
+        var productToRemove = ProductTestHelper.Default();
+        await productRepository.AddAsync(productToRemove);
+
+        // Act
+        var isRemoved = await productRepository.RemoveByIdAsync(productToRemove.Id);
+        var retrievedProduct = await productRepository.GetByIdAsync(productToRemove.Id);
+
+        // Assert
+        isRemoved.Should().BeTrue();
+        retrievedProduct.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task RemoveByIdAsync_ShouldReturnFalse_WhenProductDoesNotExist()
+    {
+        // Arrange
+        var nonExistentProductId = Guid.Empty;
+
+        // Act
+        var result = await productRepository.RemoveByIdAsync(nonExistentProductId);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
