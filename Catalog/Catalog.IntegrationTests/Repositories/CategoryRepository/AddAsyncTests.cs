d620e4449dbf098e9256984e03d407056586fa1e
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-21 19:49:25 +0300

    Add integration tests for CategoryRepository

---
+++ Catalog/Catalog.IntegrationTests/Repositories/CategoryRepository/AddAsyncTests.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Infrastructure.Database;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Catalog.IntegrationTests.SeedData;
+using FluentAssertions;
+using Xunit;
+
+namespace Catalog.IntegrationTests.Repositories.CategoryRepository;
+
+public class AddAsyncTests : IClassFixture<DatabaseFixture>
+{
+    private readonly ICategoryRepository categoryRepository;
+
+    public AddAsyncTests(DatabaseFixture fixture)
+    {
+        var context = new DbContext(fixture.ConnectionString);
+        categoryRepository = new Infrastructure.Database.Repositories.CategoryRepository(context);
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldReturnTrue_WhenCategoryIsAdded()
+    {
+        // Arrange
+        var categoryToAdd = CategoryTestHelper.Default();
+
+        // Act
+        var isAdded = await categoryRepository.AddAsync(categoryToAdd);
+        var retrievedCategory = await categoryRepository.GetByIdAsync(categoryToAdd.Id);
+
+        // Assert
+        isAdded.Should().BeTrue();
+        retrievedCategory.Should().NotBeNull();
+        retrievedCategory.Should().BeEquivalentTo(categoryToAdd);
+    }
+}
---
+++ Catalog/Catalog.IntegrationTests/Repositories/CategoryRepository/GetByIdAsyncTests.cs
@@ -0,0 +1,43 @@
+﻿using Catalog.Infrastructure.Database;
+using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Catalog.IntegrationTests.SeedData;
+using FluentAssertions;
+using Xunit;
+
+namespace Catalog.IntegrationTests.Repositories.CategoryRepository;
+
+public class GetByIdAsyncTests : IClassFixture<DatabaseFixture>
+{
+    private readonly ICategoryRepository categoryRepository;
+
+    public GetByIdAsyncTests(DatabaseFixture fixture)
+    {
+        var context = new DbContext(fixture.ConnectionString);
+        categoryRepository = new Infrastructure.Database.Repositories.CategoryRepository(context);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnCategory_WhenCategoryExists()
+    {
+        // Arrange
+        var categoryToGet = CategoryTestHelper.Default();
+        await categoryRepository.AddAsync(categoryToGet);
+
+        // Act
+        var retrievedCategory = await categoryRepository.GetByIdAsync(categoryToGet.Id);
+
+        // Assert
+        retrievedCategory.Should().NotBeNull();
+        retrievedCategory.Should().BeEquivalentTo(categoryToGet);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnNull_WhenCategoryDoesNotExist()
+    {
+        // Act
+        var retrievedCategory = await categoryRepository.GetByIdAsync(Guid.Empty);
+
+        // Assert
+        retrievedCategory.Should().BeNull();
+    }
+}
