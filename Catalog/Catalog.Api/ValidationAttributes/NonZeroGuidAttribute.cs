6d886321d8c2d0b5078d6975372bf039d813101d
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-23 15:01:56 +0300

    (#41) Fix validation attributes fail when validation context is null

--- Catalog/Catalog.Api/ValidationAttributes/NonZeroGuidAttribute.cs
+++ Catalog/Catalog.Api/ValidationAttributes/NonZeroGuidAttribute.cs
@@ -4,11 +4,13 @@ namespace Catalog.Api.ValidationAttributes;
 
 public class NonZeroGuidAttribute : ValidationAttribute
 {
-    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    protected override ValidationResult IsValid(object? value, ValidationContext? validationContext)
     {
         if (value is not Guid guidValue || guidValue == Guid.Empty)
         {
-            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+            return validationContext?.MemberName == null
+                ? new ValidationResult(ErrorMessage, null)
+                : new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
         }
 
         return ValidationResult.Success!;
