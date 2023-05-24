4ad2029a48b83faeaeb56033867ed77545bf4967
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-24 19:18:17 +0300

    Add API versioning configuration to startup

--- Catalog/Catalog.Api/Program.cs
+++ Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,9 @@
 using Catalog.Infrastructure.Database;
 using Catalog.Infrastructure.Database.Repositories;
 using Catalog.Infrastructure.Database.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -19,8 +22,28 @@ var services = builder.Services;
     services.AddControllers();
     services.AddEndpointsApiExplorer();
 
+    services.AddApiVersioning(opt =>
+    {
+        opt.DefaultApiVersion = new ApiVersion(1, 0);
+        opt.AssumeDefaultVersionWhenUnspecified = true;
+        opt.ReportApiVersions = true;
+        opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+    });
+
+    services.AddVersionedApiExplorer(setup =>
+    {
+        setup.GroupNameFormat = "'v'VVV";
+        setup.SubstituteApiVersionInUrl = true;
+    });
+
     services.AddSwaggerGen(options =>
     {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "API of Catalog microservice",
+            Version = "v1",
+        });
+
         var filePath = Path.Combine(AppContext.BaseDirectory, "Catalog.Api.xml");
         options.IncludeXmlComments(filePath);
     });
@@ -29,8 +52,8 @@ var services = builder.Services;
 
 var app = builder.Build();
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/swagger/v1/swagger.json", $"v1"); });
 
     app.MapControllers();
 }
