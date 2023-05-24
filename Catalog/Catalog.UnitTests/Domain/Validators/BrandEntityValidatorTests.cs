9c2c2cc8a7cdd69eda7427112b182a0893205db5
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-24 15:26:46 +0300

    Cover entity validators with unit tests

---
+++ Catalog/Catalog.UnitTests/Domain/Validators/BrandEntityValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using Catalog.Domain.Validators;
+using FluentAssertions;
+using Xunit;
+using BrandTestHelper = Catalog.UnitTests.SeedData.BrandTestHelper;
+
+namespace Catalog.UnitTests.Domain.Validators;
+
+public class BrandEntityValidatorTests
+{
+    private readonly BrandEntityValidator validator;
+
+    public BrandEntityValidatorTests()
+    {
+        validator = new BrandEntityValidator();
+    }
+
+    [Fact]
+    public void Validate_ValidBrandEntity_ShouldNotHaveValidationError()
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity();
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("     ")]
+    public void Validate_EmptyName_ShouldHaveValidationError(string name)
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity(name: name);
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(brandEntity.Name));
+    }
+
+    [Fact]
+    public void Validate_LongName_ShouldHaveValidationError()
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity(name: new string('A', 51));
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(brandEntity.Name));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("     ")]
+    public void Validate_EmptyDescription_ShouldHaveValidationError(string description)
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity(description: description);
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(brandEntity.Description));
+    }
+
+    [Fact]
+    public void Validate_LongDescription_ShouldHaveValidationError()
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity(description: new string('A', 1001));
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(brandEntity.Description));
+    }
+
+    [Fact]
+    public void Validate_NegativeDisplayOrder_ShouldHaveValidationError()
+    {
+        // Arrange
+        var brandEntity = BrandTestHelper.GetEntity(displayOrder: -1);
+
+        // Act
+        var result = validator.Validate(brandEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(brandEntity.DisplayOrder));
+    }
+}
---
+++ Catalog/Catalog.UnitTests/Domain/Validators/ProductImageEntityValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.Domain.Validators;
+using Catalog.IntegrationTests.SeedData;
+using FluentAssertions;
+using Xunit;
+using ProductImageTestHelper = Catalog.UnitTests.SeedData.ProductImageTestHelper;
+
+namespace Catalog.UnitTests.Domain.Validators;
+
+public class ProductImageEntityValidatorTests
+{
+    private readonly ProductImageEntityValidator validator;
+
+    public ProductImageEntityValidatorTests()
+    {
+        validator = new ProductImageEntityValidator();
+    }
+
+    [Fact]
+    public void Validate_ValidProductImageEntity_ShouldNotHaveValidationError()
+    {
+        // Arrange
+        var productImageEntity = ProductImageTestHelper.GetEntity();
+
+        // Act
+        var result = validator.Validate(productImageEntity);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validate_NegativeDisplayOrder_ShouldHaveValidationError()
+    {
+        // Arrange
+        var productImageEntity = ProductImageTestHelper.GetEntity(displayOrder: -1);
+
+        // Act
+        var result = validator.Validate(productImageEntity);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == nameof(productImageEntity.DisplayOrder));
+    }
+}
