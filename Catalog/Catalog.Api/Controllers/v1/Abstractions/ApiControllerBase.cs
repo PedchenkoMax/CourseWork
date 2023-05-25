c00ca671d4deceb2d7d763bec1e930a2b57d6bf9
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-25 14:39:29 +0300

    (#42) Refactor ApiControllerBase methods

--- Catalog/Catalog.Api/Controllers/v1/Abstractions/ApiControllerBase.cs
+++ Catalog/Catalog.Api/Controllers/v1/Abstractions/ApiControllerBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
+using System.Net;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,45 +9,37 @@ public class ApiControllerBase<TController> : ControllerBase where TController :
 {
     protected BadRequestObjectResult BadRequest(ValidationResult validationResult)
     {
-        const string type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
-        const string title = "One or more validation errors occurred.";
-        const int status = StatusCodes.Status400BadRequest;
+        var problemDetails = CreateProblemDetails(StatusCodes.Status400BadRequest);
 
-        var errors = validationResult
-                     .Errors
-                     .ToLookup(f => f.PropertyName, f => f.ErrorMessage)
-                     .ToDictionary(group => group.Key, group => group.ToArray());
-
-        var problemDetails = CreateProblemDetails(type, title, status, errors);
+        problemDetails.Extensions["errors"] = validationResult
+                                              .Errors
+                                              .ToLookup(f => f.PropertyName, f => f.ErrorMessage)
+                                              .ToDictionary(group => group.Key, group => group.ToArray());
 
         return BadRequest(problemDetails);
     }
 
     protected NotFoundObjectResult NotFound(string fieldName)
     {
-        const string type = "https://tools.ietf.org/html/rfc7231#section-6.5.4";
-        const string title = "Not Found";
-        const int status = StatusCodes.Status404NotFound;
+        var problemDetails = CreateProblemDetails(StatusCodes.Status404NotFound);
 
-        var errors = new Dictionary<string, string[]> { { fieldName, new[] { "The requested resource was not found." } } };
-
-        var problemDetails = CreateProblemDetails(type, title, status, errors);
+        problemDetails.Extensions["errors"] = new Dictionary<string, string[]>
+            { { fieldName, new[] { "The requested resource was not found." } } };
 
         return NotFound(problemDetails);
     }
 
-    private ProblemDetails CreateProblemDetails(string type, string title, int status, object errors)
+    private ProblemDetails CreateProblemDetails(int status)
     {
-        return new ProblemDetails
+        var problemDetails = new ProblemDetails
         {
-            Type = type,
-            Title = title,
             Status = status,
             Extensions =
             {
-                ["traceId"] = Activity.Current?.Id ?? HttpContext?.TraceIdentifier,
-                ["errors"] = errors,
+                ["traceId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
             },
         };
+
+        return problemDetails;
     }
 }
