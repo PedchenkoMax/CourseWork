414ae62cd8ec4d7b75cfa03f06a2fae6c7002e04
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-25 22:03:27 +0300

    Add CategorySchema with table name and column names of the  table in the database
    And update CategoryRepository to use these constants instead of hardcoded values

---
+++ Catalog/Catalog.Infrastructure/Database/Schemas/CategorySchema.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Infrastructure.Database.Schemas;
+
+public static class CategorySchema
+{
+    public static string Table => "categories";
+
+    public static class Columns
+    {
+        public static string Id => "id";
+        public static string ParentCategoryId => "parent_category_id";
+        public static string Name => "name";
+        public static string Description => "description";
+        public static string ImageUrl => "image_url";
+        public static string DisplayOrder => "display_order";
+    }
+}
