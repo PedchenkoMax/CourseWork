2f5446a646bd4b92e3e03bd20e83536d7c72d24b
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-05-25 22:02:39 +0300

    Add BrandSchema with table name and column names of the table in the database
    And update BrandRepository to use these constants instead of hardcoded values.

---
+++ Catalog/Catalog.Infrastructure/Database/Schemas/BrandSchema.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.Infrastructure.Database.Schemas;
+
+public static class BrandSchema
+{
+    public static string Table => "brands";
+
+    public static class Columns
+    {
+        public static string Id => "id";
+        public static string Name => "name";
+        public static string Description => "description";
+        public static string ImageUrl => "image_url";
+        public static string DisplayOrder => "display_order";
+    }
+}
