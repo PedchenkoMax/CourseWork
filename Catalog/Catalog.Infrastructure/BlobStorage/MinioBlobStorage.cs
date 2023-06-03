6a8e5798739754c80269d063e05a9d7f9ff3df74
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-06-03 20:42:29 +0300

    Cover blob-related services with logs

--- Catalog/Catalog.Infrastructure/BlobStorage/MinioBlobStorage.cs
+++ Catalog/Catalog.Infrastructure/BlobStorage/MinioBlobStorage.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.Infrastructure.BlobStorage.Abstractions;
+using Microsoft.Extensions.Logging;
 using Minio;
 using Minio.Exceptions;
 
@@ -6,10 +7,12 @@ namespace Catalog.Infrastructure.BlobStorage;
 
 public class MinioBlobStorage : IBlobStorage
 {
+    private readonly ILogger<MinioBlobStorage> logger;
     private readonly MinioClient minioClient;
 
-    public MinioBlobStorage(MinioClient minioClient)
+    public MinioBlobStorage(ILogger<MinioBlobStorage> logger, MinioClient minioClient)
     {
+        this.logger = logger;
         this.minioClient = minioClient;
     }
 
@@ -17,6 +20,8 @@ public class MinioBlobStorage : IBlobStorage
     {
         try
         {
+            logger.LogInformation("Uploading file {UniqueFileName} to Minio bucket {BucketName}...", uniqueFileName, bucketName);
+
             var putObjectArgs = new PutObjectArgs()
                                 .WithBucket(bucketName)
                                 .WithStreamData(stream)
@@ -25,10 +30,11 @@ public class MinioBlobStorage : IBlobStorage
                                 .WithObject(uniqueFileName);
 
             await minioClient.PutObjectAsync(putObjectArgs);
+            logger.LogInformation("File {UniqueFileName} uploaded successfully to Minio bucket {BucketName}", uniqueFileName, bucketName);
         }
         catch (MinioException ex)
         {
-            // log here
+            logger.LogError(ex, "Error occurred while uploading file {UniqueFileName} to Minio bucket {BucketName}", uniqueFileName, bucketName);
             throw;
         }
     }
@@ -37,15 +43,17 @@ public class MinioBlobStorage : IBlobStorage
     {
         try
         {
+            logger.LogInformation("Deleting file {FileName} from Minio bucket {BucketName}...", fileName, bucketName);
             var removeObjectArgs = new RemoveObjectArgs()
                                    .WithBucket(bucketName)
                                    .WithObject(fileName);
 
             await minioClient.RemoveObjectAsync(removeObjectArgs);
+            logger.LogInformation("File {FileName} deleted successfully from Minio bucket {BucketName}", fileName, bucketName);
         }
         catch (MinioException ex)
         {
-            // log here
+            logger.LogError(ex, "Error occurred while deleting file {FileName} from Minio bucket {BucketName}", fileName, bucketName);
             throw;
         }
     }
