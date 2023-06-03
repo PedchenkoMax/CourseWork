7d8c790ac302cf775e1aa030de5a7fff6b2a8674
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-06-03 04:39:32 +0300

    Reorder rules so migration is valid

--- Catalog/Catalog.Infrastructure/Database/Migrations/Initial.cs
+++ Catalog/Catalog.Infrastructure/Database/Migrations/Initial.cs
@@ -9,7 +9,7 @@ public class Initial : Migration
 {
     public override void Up()
     {
-        #region Product
+        #region Tables
 
         Create.Table(ProductSchema.Table)
               .WithColumn(ProductSchema.Columns.Id).AsGuid().PrimaryKey()
@@ -24,6 +24,28 @@ public class Initial : Migration
               .WithColumn(ProductSchema.Columns.Stock).AsInt32().NotNullable()
               .WithColumn(ProductSchema.Columns.Availability).AsBoolean().NotNullable();
 
+        Create.Table(ProductImageSchema.Table)
+              .WithColumn(ProductImageSchema.Columns.Id).AsGuid().PrimaryKey()
+              .WithColumn(ProductImageSchema.Columns.ProductId).AsGuid().NotNullable()
+              .WithColumn(ProductImageSchema.Columns.ImageFileName).AsString().NotNullable()
+              .WithColumn(ProductImageSchema.Columns.DisplayOrder).AsInt32().NotNullable();
+
+        Create.Table(CategorySchema.Table)
+              .WithColumn(CategorySchema.Columns.Id).AsGuid().PrimaryKey()
+              .WithColumn(CategorySchema.Columns.ParentCategoryId).AsGuid().Nullable()
+              .WithColumn(CategorySchema.Columns.Name).AsString().NotNullable()
+              .WithColumn(CategorySchema.Columns.Description).AsString().NotNullable()
+              .WithColumn(CategorySchema.Columns.ImageFileName).AsString().Nullable();
+
+        Create.Table(BrandSchema.Table)
+              .WithColumn(BrandSchema.Columns.Id).AsGuid().PrimaryKey()
+              .WithColumn(BrandSchema.Columns.Name).AsString().NotNullable()
+              .WithColumn(BrandSchema.Columns.Description).AsString().NotNullable()
+              .WithColumn(BrandSchema.Columns.ImageFileName).AsString().Nullable();
+
+        #endregion
+
+        #region Foreign Keys
 
         Create.ForeignKey("fk_products_brands")
               .FromTable(ProductSchema.Table).ForeignColumn(ProductSchema.Columns.BrandId)
@@ -35,6 +57,19 @@ public class Initial : Migration
               .ToTable(CategorySchema.Table).PrimaryColumn(CategorySchema.Columns.Id)
               .OnDelete(Rule.SetNull);
 
+        Create.ForeignKey("fk_product_images_products")
+              .FromTable(ProductImageSchema.Table).ForeignColumn(ProductImageSchema.Columns.ProductId)
+              .ToTable(ProductSchema.Table).PrimaryColumn(ProductSchema.Columns.Id)
+              .OnDelete(Rule.Cascade);
+
+        Create.ForeignKey("fk_categories_parent_categories")
+              .FromTable(CategorySchema.Table).ForeignColumn(CategorySchema.Columns.ParentCategoryId)
+              .ToTable(CategorySchema.Table).PrimaryColumn(CategorySchema.Columns.Id)
+              .OnDelete(Rule.SetNull);
+
+        #endregion
+
+        #region Indexes
 
         Create.Index("idx_products_brand_id")
               .OnTable(ProductSchema.Table)
@@ -61,60 +96,15 @@ public class Initial : Migration
               .OnColumn(ProductSchema.Columns.Discount)
               .Ascending();
 
-        #endregion
-
-        #region Product Image
-
-        Create.Table(ProductImageSchema.Table)
-              .WithColumn(ProductImageSchema.Columns.Id).AsGuid().PrimaryKey()
-              .WithColumn(ProductImageSchema.Columns.ProductId).AsGuid().NotNullable()
-              .WithColumn(ProductImageSchema.Columns.ImageFileName).AsString().NotNullable()
-              .WithColumn(ProductImageSchema.Columns.DisplayOrder).AsInt32().NotNullable();
-
-
-        Create.ForeignKey("fk_product_images_products")
-              .FromTable(ProductImageSchema.Table).ForeignColumn(ProductImageSchema.Columns.ProductId)
-              .ToTable(ProductSchema.Table).PrimaryColumn(ProductSchema.Columns.Id)
-              .OnDelete(Rule.Cascade);
-
-
         Create.Index("idx_product_images_product_id")
               .OnTable(ProductImageSchema.Table)
               .OnColumn(ProductImageSchema.Columns.ProductId)
               .Ascending();
 
-        #endregion
-
-        #region Category
-
-        Create.Table(CategorySchema.Table)
-              .WithColumn(CategorySchema.Columns.Id).AsGuid().PrimaryKey()
-              .WithColumn(CategorySchema.Columns.ParentCategoryId).AsGuid().Nullable()
-              .WithColumn(CategorySchema.Columns.Name).AsString().NotNullable()
-              .WithColumn(CategorySchema.Columns.Description).AsString().NotNullable()
-              .WithColumn(CategorySchema.Columns.ImageFileName).AsString().Nullable();
-
-
-        Create.ForeignKey("fk_categories_parent_categories")
-              .FromTable(CategorySchema.Table).ForeignColumn(CategorySchema.Columns.ParentCategoryId)
-              .ToTable(CategorySchema.Table).PrimaryColumn(CategorySchema.Columns.Id)
-              .OnDelete(Rule.SetNull);
-
-
         Create.Index("idx_categories_parent_category_id")
               .OnTable(CategorySchema.Table)
               .OnColumn(CategorySchema.Columns.ParentCategoryId)
               .Ascending();
-
-        #endregion
-
-        #region Brand
-
-        Create.Table(BrandSchema.Table)
-              .WithColumn(BrandSchema.Columns.Id).AsGuid().PrimaryKey()
-              .WithColumn(BrandSchema.Columns.Name).AsString().NotNullable()
-              .WithColumn(BrandSchema.Columns.Description).AsString().NotNullable()
-              .WithColumn(BrandSchema.Columns.ImageFileName).AsString().Nullable();
 
         #endregion
     }
