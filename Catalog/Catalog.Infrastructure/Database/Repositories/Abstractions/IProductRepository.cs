80e4a16168b2f53a3fe754bfafd1bd40ebf5a438
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-06-04 03:19:31 +0300

    Add search by parameters functionality to product repository

--- Catalog/Catalog.Infrastructure/Database/Repositories/Abstractions/IProductRepository.cs
+++ Catalog/Catalog.Infrastructure/Database/Repositories/Abstractions/IProductRepository.cs
@@ -6,6 +6,7 @@ namespace Catalog.Infrastructure.Database.Repositories.Abstractions;
 public interface IProductRepository
 {
     IDbTransaction BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
+    Task<(List<ProductEntity>, int)> SearchByParametersAsync(int pageNumber, int pageSize, string orderBy, bool isAscending);
     Task<List<ProductEntity>> GetAllAsync();
     Task<ProductEntity?> GetByIdAsync(Guid id);
     Task<bool> UpdateAsync(ProductEntity product);
