72e52057ba1c613faed0620b3ec49f0f96a99ed0
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-06-06 01:04:02 +0300

    Catch all exceptions in MinioBlobStorage and rethrow as BlobStorageException

---
+++ Catalog/Catalog.Infrastructure/Exceptions/BlobStorageException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Infrastructure.Exceptions;
+
+public class BlobStorageException : Exception
+{
+    public BlobStorageException(Exception innerException) 
+        : base("An error occurred with BlobStorage.", innerException)
+    {
+    }
+}
