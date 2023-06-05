b4e7df5ea7ff6283f83b09913fb7678791f2a658
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-06-06 00:50:06 +0300

    Separate configurations into individual methods

--- Catalog/Catalog.Api/Program.cs
+++ Catalog/Catalog.Api/Program.cs
@@ -27,12 +27,47 @@ using StackExchange.Redis;
 
 namespace Catalog.Api;
 
-internal class Program
+internal static class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        ConfigureLogger(builder);
+
+        var services = builder.Services;
+        {
+            ConfigureHealthChecks(services, builder.Configuration);
+
+            ConfigureDatabase(services, builder.Configuration);
+            ConfigureCache(services, builder.Configuration);
+
+            ConfigureMassTransit(services, builder.Configuration);
+            ConfigureBlob(services, builder.Configuration);
+
+            ConfigureSwagger(services);
+            ConfigureMappers(services);
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        var app = builder.Build();
+        {
+            app.UseExceptionHandlingMiddleware();
+            app.UseSerilogRequestLogging();
+
+            app.MapControllers();
+
+            ConfigureSwaggerUI(app);
+            ConfigureHealthChecksEndpoints(app);
+        }
+
+        app.Run();
+    }
+
+    private static void ConfigureLogger(WebApplicationBuilder builder)
+    {
         builder.Host.UseSerilog((hostingContext, loggerConfig) =>
         {
             loggerConfig.ReadFrom.Configuration(hostingContext.Configuration)
@@ -40,149 +75,153 @@ internal class Program
                         .Enrich.WithMachineName()
                         .Enrich.WithThreadId();
         });
+    }
 
-        var configuration = builder.Configuration;
+    private static void ConfigureHealthChecks(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHealthChecks()
+                .AddNpgSql(
+                    npgsqlConnectionString: configuration["PostgresConnectionString"]!,
+                    name: "PostgresSQL",
+                    failureStatus: HealthStatus.Unhealthy)
+                .AddRedis(
+                    redisConnectionString: configuration["RedisConnectionString"]!,
+                    name: "Redis",
+                    failureStatus: HealthStatus.Unhealthy)
+                .AddMinio(
+                    factory: sp => sp.GetRequiredService<MinioClient>(),
+                    name: "Minio",
+                    failureStatus: HealthStatus.Unhealthy)
+                .AddRabbitMQ(
+                    rabbitConnectionString: configuration["RabbitMqConnectionString"]!,
+                    name: "RabbitMq",
+                    failureStatus: HealthStatus.Unhealthy);
 
-        var services = builder.Services;
+        services.AddHealthChecksUI()
+                .AddInMemoryStorage();
+    }
+
+    private static void ConfigureDatabase(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<DapperDbContext>(_ => new DapperDbContext(configuration["PostgresConnectionString"]!));
+        services.AddTransient<IProductRepository, ProductRepository>();
+        services.AddTransient<IProductImageRepository, ProductImageRepository>();
+        services.AddTransient<IBrandRepository, BrandRepository>();
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+
+        var migrationRunner = new MigrationRunner();
+        migrationRunner.RunMigrations(configuration["PostgresConnectionString"]!, true);
+    }
+
+    private static void ConfigureCache(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IConnectionMultiplexer>(x => ConnectionMultiplexer.Connect(configuration["RedisConnectionString"]!));
+        services.AddSingleton<RedisCacheManager>();
+        services.Decorate<IProductRepository, CachedProductRepository>();
+        services.Decorate<IProductImageRepository, CachedProductImageRepository>();
+        services.Decorate<IBrandRepository, CachedBrandRepository>();
+        services.Decorate<ICategoryRepository, CachedCategoryRepository>();
+    }
+
+    private static void ConfigureMassTransit(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(configurator =>
         {
-            services.AddMassTransit(configurator =>
+            configurator.SetKebabCaseEndpointNameFormatter();
+
+            configurator.AddConsumers(Assembly.GetExecutingAssembly());
+
+            configurator.UsingRabbitMq((context, busConfigurator) =>
             {
-                configurator.SetKebabCaseEndpointNameFormatter();
+                busConfigurator.ConfigureEndpoints(context);
 
                 configurator.AddConsumers(Assembly.GetExecutingAssembly());
 
-                configurator.UsingRabbitMq((context, busConfigurator) =>
+                busConfigurator.Host(configuration["RabbitMqConnectionString"]);
+
+                busConfigurator.UseRetry(retryConfig =>
                 {
-                    busConfigurator.ConfigureEndpoints(context);
-
-                    configurator.AddConsumers(Assembly.GetExecutingAssembly());
-
-                    busConfigurator.Host(configuration["RabbitMqConnectionString"]);
-
-                    busConfigurator.UseRetry(retryConfig =>
-                    {
-                        retryConfig.Interval(10, TimeSpan.FromSeconds(30));
-                        retryConfig.Handle<BrokerUnreachableException>();
-                    });
+                    retryConfig.Interval(10, TimeSpan.FromSeconds(30));
+                    retryConfig.Handle<BrokerUnreachableException>();
                 });
             });
+        });
+    }
 
-            services.AddSingleton<DapperDbContext>(_ => new DapperDbContext(configuration["PostgresConnectionString"]!));
-            services.AddSingleton<IConnectionMultiplexer>(x => ConnectionMultiplexer.Connect(configuration["RedisConnectionString"]!));
-            services.AddSingleton<RedisCacheManager>();
-
-            services.AddTransient<IProductRepository, ProductRepository>();
-            services.Decorate<IProductRepository, CachedProductRepository>();
-
-            services.AddTransient<IProductImageRepository, ProductImageRepository>();
-            services.Decorate<IProductImageRepository, CachedProductImageRepository>();
-
-            services.AddTransient<IBrandRepository, BrandRepository>();
-            services.Decorate<IBrandRepository, CachedBrandRepository>();
-
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-            services.Decorate<ICategoryRepository, CachedCategoryRepository>();
-
-            services.AddMinio(options =>
-            {
-                options.Endpoint = configuration["MinioOptions:Endpoint"]!;
-                options.AccessKey = configuration["MinioOptions:AccessKey"]!;
-                options.SecretKey = configuration["MinioOptions:SecretKey"]!;
-            });
-
-            services.AddHealthChecks()
-                    .AddNpgSql(
-                        npgsqlConnectionString: configuration["PostgresConnectionString"],
-                        name: "PostgresSQL",
-                        failureStatus: HealthStatus.Unhealthy)
-                    .AddRedis(
-                        redisConnectionString: configuration["RedisConnectionString"],
-                        name: "Redis",
-                        failureStatus: HealthStatus.Unhealthy)
-                    .AddMinio(
-                        factory: sp => sp.GetRequiredService<MinioClient>(),
-                        name: "Minio",
-                        failureStatus: HealthStatus.Unhealthy)
-                    .AddRabbitMQ(
-                        rabbitConnectionString: configuration["RabbitMqConnectionString"],
-                        name: "RabbitMq",
-                        failureStatus: HealthStatus.Unhealthy);
-
-            services.AddHealthChecksUI()
-                    .AddInMemoryStorage();
-
-            var blobServiceSettings = configuration.GetSection("MinioBlobServiceSettings").Get<BlobServiceSettings>()!;
-            services.AddSingleton<IBlobServiceSettings, BlobServiceSettings>(_ => blobServiceSettings);
-
-            var imageHandlingSettings = configuration.GetSection("ImageHandlingSettings").Get<ImageHandlingSettings>()!;
-            services.AddSingleton<IImageHandlingSettings, ImageHandlingSettings>(_ => imageHandlingSettings);
-
-            services.AddTransient<IBlobStorage, MinioBlobStorage>();
-            services.AddTransient<IBlobService, BlobService>();
-
-            var migrationRunner = new MigrationRunner();
-            migrationRunner.RunMigrations(configuration["PostgresConnectionString"]!, true);
-
-            services.AddSingleton<IBrandMapper, BrandMapper>();
-            services.AddSingleton<ICategoryMapper, CategoryMapper>();
-            services.AddSingleton<IProductImageMapper, ProductImageMapper>();
-            services.AddSingleton<IProductMapper, ProductMapper>();
-
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-
-            services.AddApiVersioning(opt =>
-            {
-                opt.DefaultApiVersion = new ApiVersion(1, 0);
-                opt.AssumeDefaultVersionWhenUnspecified = true;
-                opt.ReportApiVersions = true;
-                opt.ApiVersionReader = new UrlSegmentApiVersionReader();
-            });
-
-            services.AddVersionedApiExplorer(setup =>
-            {
-                setup.GroupNameFormat = "'v'VVV";
-                setup.SubstituteApiVersionInUrl = true;
-            });
-
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "API of Catalog microservice",
-                    Version = "v1",
-                });
-
-                var filePath = Path.Combine(AppContext.BaseDirectory, "Catalog.Api.xml");
-                options.IncludeXmlComments(filePath);
-            });
-        }
-
-
-        var app = builder.Build();
+    private static void ConfigureBlob(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMinio(options =>
         {
-            app.UseSerilogRequestLogging();
+            options.Endpoint = configuration["MinioOptions:Endpoint"]!;
+            options.AccessKey = configuration["MinioOptions:AccessKey"]!;
+            options.SecretKey = configuration["MinioOptions:SecretKey"]!;
+        });
 
-            app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
-            app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/swagger/v1/swagger.json", $"v1"); });
+        var blobServiceSettings = configuration.GetSection("MinioBlobServiceSettings").Get<BlobServiceSettings>()!;
+        var imageHandlingSettings = configuration.GetSection("ImageHandlingSettings").Get<ImageHandlingSettings>()!;
 
-            app.UseHealthChecks("/healthz", new HealthCheckOptions
+        services.AddSingleton<IBlobServiceSettings, BlobServiceSettings>(_ => blobServiceSettings);
+        services.AddSingleton<IImageHandlingSettings, ImageHandlingSettings>(_ => imageHandlingSettings);
+
+        services.AddTransient<IBlobStorage, MinioBlobStorage>();
+        services.AddTransient<IBlobService, BlobService>();
+    }
+
+    private static void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddApiVersioning(opt =>
+        {
+            opt.DefaultApiVersion = new ApiVersion(1, 0);
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.ReportApiVersions = true;
+            opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+        });
+
+        services.AddVersionedApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
-                Predicate = _ => true,
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                Title = "API of Catalog microservice",
+                Version = "v1",
             });
 
-            app.UseHealthChecksUI(config =>
-            {
-                config.UIPath = "/health-ui";
-                config.ApiPath = "/health-ui-api";
-            });
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Catalog.Api.xml");
+            options.IncludeXmlComments(filePath);
+        });
+    }
 
-            app.UseExceptionHandlingMiddleware();
+    private static void ConfigureMappers(IServiceCollection services)
+    {
+        services.AddSingleton<IBrandMapper, BrandMapper>();
+        services.AddSingleton<ICategoryMapper, CategoryMapper>();
+        services.AddSingleton<IProductImageMapper, ProductImageMapper>();
+        services.AddSingleton<IProductMapper, ProductMapper>();
+    }
 
-            app.MapControllers();
-        }
+    private static void ConfigureSwaggerUI(IApplicationBuilder app)
+    {
+        app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
+        app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/swagger/v1/swagger.json", $"v1"); });
+    }
 
-        app.Run();
+    private static void ConfigureHealthChecksEndpoints(IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/healthz", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        app.UseHealthChecksUI(config =>
+        {
+            config.UIPath = "/health-ui";
+            config.ApiPath = "/health-ui-api";
+        });
     }
 }
